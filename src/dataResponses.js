24637908f789ff5e6aed597fd414a96a44cb85dd
Sgedelta <Sgeaston04@gmail.com>
2024-10-06 17:49:15 -0400

    finish up work for checkpoint in dataResponses, additional cleaning and a bit of parsing, comments about other methods

--- src/dataResponses.js
+++ src/dataResponses.js
@@ -47,23 +47,31 @@ const getCardByName = (request, response) => {
         return respond(request, response, 400, responseJSON, 'application/json');
     }
 
-    responseJSON.message = `Search Term Was: ${searchTerm}`; //temp
+    const foundCards = data.data.cards.filter((card) => card.name.includes(searchTerm)); //this is all of the cards with the EXACT search term included in the "Name" field of the card
+    // In the future, we can expand this to a lot of or conditions and split up the search term by commas or quotes or whatever. Make it more robust.
+    
+    //responseJSON.message = `Filtered Cards Are: ${JSON.stringify(foundCards)}`; //temporary response of just.. all the data as a string.
+    // This is where in the future we can return the data OR return a meaningful message if we didn't find anything (Filter came back as zero)
 
-    respond(request, response, 200, responseJSON, 'application/json'); //TEMP
+    responseJSON.message = foundCards; // FOR TESTING: Returns the object itself to inspect the JSON of it
+
+    respond(request, response, 200, responseJSON, 'application/json'); //TEMP (?)
 };
 module.exports.getCardByName = getCardByName;
 
 const getCardByKeyword = (request, response) => {
-
+    //similar implementation to Card By Name -> instead of checking card.name check card.keywords or MAYBE card.text // card.originalText?
 };
 module.exports.getCardByKeyword = getCardByKeyword;
 
 const getRandomBooster = (request, response) => {
-
+    //get information about a set of random cards that would represent a play booster - requires us to get a number of cards by their rarity/foil/whatever and then generate and pick randoms
+        // The rarity/foil/whatever won't change, so we could generate them as const before this is called for effeciency, then just pull random, but they won't include any "added" cards that we POST
+        //  so it's probably better to NOT do that and generate it on function call, even though that's not great with .filter() effeciency....
 };
 module.exports.getRandomBooster = getRandomBooster;
 
-const addUser = (request, response) => {
+const addUser = (request, response) => { //TEMP: Add User is still here so we can quickly generate post methods without referencing other things. Will be removed. 
     const responseJSON = {
         message: 'Name and Age are both required',
     };
